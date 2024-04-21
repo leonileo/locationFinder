ba0fd0d71bb349a7fe6f3fce105e12064b5838b8
kaleb <leolittleprogrammer@gmail.com>
2024-04-21 14:59:35 +0300

    Location finder

---
+++ main.js
@@ -0,0 +1,24 @@
+const findMyState = () => {
+
+    const status = document.querySelector('.status');
+    
+    const success = (position) => {
+        console.log(position)
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        
+        const geoApiUrl = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`;
+        
+        fetch(geoApiUrl)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        status.textContent = data.principalSubdivision
+        })
+    }
+    const error = () => {
+        status.textContent = 'Unable to retrieve your location';
+    }     
+        navigator.geolocation.getCurrentPosition(success, error);
+}
+document.querySelector('.find-state').addEventListener('click', findMyState);
