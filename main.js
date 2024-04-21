c2376f79620c76ef16b3f55d2529c3bc056b9ace
kaleb <leolittleprogrammer@gmail.com>
2024-04-21 17:59:24 +0300

    Locationfinder

--- main.js
+++ main.js
@@ -1,21 +1,16 @@
 const findMyState = () => {
-
     const status = document.querySelector('.status');
     const place = document.querySelector('.place');
-//     navigator.permissions
-//     .query({ name: "geolocation" })
-//     .then((permissionStatus) => {
-//       console.log(`geolocation permission status is ${permissionStatus.state}`);
-//       permissionStatus.state = "granted"
-//     //   navigator.geolocation.getCurrentPosition(success, error)
-// });
+    var lon;
+    var lan;
 
-const success = (position) => {
-        console.log(position)
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-        
-        const geoApiUrl = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`;
+    fetch('https://ipapi.co/json/')
+    .then(function(response) {
+    response.json().then(jsonData => {
+        lon = jsonData.longitude
+        lan = jsonData.latitude
+
+        const geoApiUrl = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lan}&longitude=${lon}&localityLanguage=en`;
         
         fetch(geoApiUrl)
         .then(res => res.json())
@@ -24,11 +19,12 @@ const success = (position) => {
             status.textContent = data.principalSubdivision + ", "
             place.textContent = data.locality
         })
-    }
-    const error = () => {
-        status.textContent = 'Unable to retrieve your location';
-    }         
-    console.log(navigator.geolocation.watchPosition(success))
-    }
-    document.querySelector('.find-state').addEventListener('click', findMyState);
-    findMyState()
+    });
+})
+
+        
+}
+
+document.querySelector('.find-state').addEventListener('click', findMyState);
+
+// findMyState()
